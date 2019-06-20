8d7a2ef71fbd67f5937f5eb0871a4bf9ca807b24
Jaiprakashrenukumar <rjai.jp19@gmail.com>
2019-06-20 17:59:20 +0530

    Reading Multiple Values

---
+++ Script/input_script3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo -n "enter your first name: "
+read firstname
+echo -n "enter your last name: "
+read Lastname
+echo -n "enter the location you come from: "
+read area
+Fullname="$firstname $Lastname"
+Message="well $Fullname, coming from $area welcome to HCL!!!!"
+echo  $Message
+echo "your are appointed as Asterisk Engineer"
+  
+
+
---
+++ Script/script2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "how is login to the system"
+echo -n "sudo user: "
+whoami
