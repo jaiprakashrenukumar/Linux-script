26b31be02792a28a0fd739ca60cac0cd98525b2e
jaiprakashrenukumar <rjai.jp19@gmail.com>
2020-07-01 07:20:11 +0000

    basic shell program

---
+++ shell_script/01_syntax_check.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+Script: check syntax and debug the script
+
+#set -x uncomment to check the syntax 
+
+#set -n uncomment to debug this script
+
+for x in {1..10}
+do
+echo $x
+done
